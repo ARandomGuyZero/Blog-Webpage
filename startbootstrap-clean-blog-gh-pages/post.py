d1e4b890d63606ee68078b5cd4d0f48cabb21fec
Alan Ramos López <118926925+ARandomGuyZero@users.noreply.github.com>
2024-10-16 19:23:50 -0700

    Delete startbootstrap-clean-blog-gh-pages directory

--- startbootstrap-clean-blog-gh-pages/post.py
+++
@@ -1,7 +0,0 @@
-class Post:
-    def __init__(self, post_id, body, title, subtitle, image_url):
-        self.id = post_id
-        self.body = body
-        self.title = title
-        self.subtitle = subtitle
-        self.image_url = image_url
