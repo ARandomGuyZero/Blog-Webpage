c79c9c948b8438447359587ec769a9d93e259798
Alan Ramos López <118926925+ARandomGuyZero@users.noreply.github.com>
2024-10-30 14:36:33 -0700

    Now uses data from database

--- main.py
+++ main.py
@@ -8,33 +8,48 @@ This application uses Flask to show a webpage that uses Bootstrap for design and
 """
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-
-from flask import Flask, render_template, request
-from requests import get
-from post import Post
 from smtplib import SMTP
 
-EMAIL = "email@gmail.com" # Your email
-PASSWORD = "password" # Your password
-HOST = "smtp.gmail.com" # I use gmail's smtp, may change depending on your email provider
-PORT = 587
+from flask import Flask, render_template
+from flask import request
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, String, Text
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
-# Gets the data of the post
-posts = get(url="https://api.npoint.io/dfae0d41493794c77072").json()
+EMAIL = "email@gmail.com"  # Your email
+PASSWORD = "password"  # Your password
+HOST = "smtp.gmail.com"  # I use gmail's smtp, may change depending on your email provider
+PORT = 587
 
 # Sets a post list empty
 post_list = []
 
-# For each post in post
-for post in posts:
-
-    # Save a new post object using data from the json
-    post_obj = Post(post["id"], post["body"], post["title"], post["subtitle"], post["image_url"])
-
-    # And then append it to the post list
-    post_list.append(post_obj)
-
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
+
+
+# CREATE DATABASE
+class Base(DeclarativeBase):
+    pass
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+# CONFIGURE TABLE
+class BlogPost(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
+    date: Mapped[str] = mapped_column(String(250), nullable=False)
+    body: Mapped[str] = mapped_column(Text, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
 
 @app.route('/')
 def index():
@@ -42,7 +57,9 @@ def index():
     Renders index.html file
     :return:
     """
-    return render_template("index.html", posts=posts)
+    blogposts = db.session.execute(db.select(BlogPost)).scalars().all()
+    return render_template("index.html", posts=blogposts)
+
 
 @app.route('/post/<int:index_number>')
 def post(index_number):
@@ -52,20 +69,9 @@ def post(index_number):
     :return:
     """
 
-    # Set none as default
-    required_post = None
+    blogpost = db.get_or_404(BlogPost, index_number)
+    return render_template("post.html", post=blogpost)
 
-    # For each post item in the post_list
-    for post_item in post_list:
-
-        # If the post_item id is the same as the index_number, will save the required post as an index
-        if post_item.id == index_number:
-
-            # Store the required post
-            required_post = post_item
-            break
-
-    return render_template("post.html", post=required_post)
 
 @app.route('/about')
 def about():
@@ -74,6 +80,7 @@ def about():
     :return:
     """
     return render_template("about.html")
+
 
 @app.route('/contact', methods=["GET", "POST"])
 def contact():
@@ -95,7 +102,6 @@ def contact():
         msg.attach(MIMEText(body, 'plain'))
 
         with SMTP(host=HOST, port=PORT) as smtp:
-
             smtp.starttls()
 
             smtp.login(user=EMAIL, password=PASSWORD)
@@ -107,6 +113,7 @@ def contact():
         return render_template("contact.html", message_sent=message_sent)
 
     return render_template("contact.html", message_sent=message_sent)
+
 
 # Start the flask application
 if __name__ == "__main__":
