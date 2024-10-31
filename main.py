5867210e8a622d8d582ca0156e20c038ba6bff86
Alan Ramos López <118926925+ARandomGuyZero@users.noreply.github.com>
2024-10-30 17:30:00 -0700

    Added the ability to create new posts

--- main.py
+++ main.py
@@ -6,16 +6,21 @@ Date: October 16th 2024
 
 This application uses Flask to show a webpage that uses Bootstrap for design and a json for the data.
 """
+from datetime import datetime
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from smtplib import SMTP
 
-from flask import Flask, render_template
-from flask import request
+from flask import Flask, render_template, request, url_for
 from flask_bootstrap import Bootstrap5
+from flask_ckeditor import CKEditor, CKEditorField
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
 from sqlalchemy import Integer, String, Text
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from werkzeug.utils import redirect
+from wtforms.fields.simple import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
 
 EMAIL = "email@gmail.com"  # Your email
 PASSWORD = "password"  # Your password
@@ -27,6 +32,7 @@ post_list = []
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+ckeditor = CKEditor(app)
 Bootstrap5(app)
 
 
@@ -35,9 +41,19 @@ class Base(DeclarativeBase):
     pass
 
 
+# Were we will save the data
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
+
+
+class PostForm(FlaskForm):
+    title = StringField("Blog Post Title:", validators=[DataRequired()])
+    subtitle = StringField("Blog Post Subtitle:", validators=[DataRequired()])
+    author = StringField("Your Name:", validators=[DataRequired()])
+    img_url = StringField("The Image URL for your Blog:", validators=[DataRequired(), URL()])
+    body = CKEditorField("Body:", validators=[DataRequired()])
+    submit = SubmitField()
 
 
 # CONFIGURE TABLE
@@ -71,6 +87,34 @@ def post(index_number):
 
     blogpost = db.get_or_404(BlogPost, index_number)
     return render_template("post.html", post=blogpost)
+
+
+@app.route("/new-post", methods=["GET", "POST"])
+def add_new_post():
+    form = PostForm()
+
+    # If the user clicks on submit
+    if request.method == "POST":
+        # Gets the server date in a formatted code
+        date = datetime.now().strftime("%m %d, %Y")
+        # Creates a new post using the form's data
+        new_post = BlogPost(
+            title=request.form.get("title"),
+            subtitle=request.form.get("subtitle"),
+            body=request.form.get("body"),
+            date=date,
+            author=request.form.get("author"),
+            img_url=request.form.get("img_url"),
+        )
+        # Adds the new row to the database
+        db.session.add(new_post)
+
+        # Saves changes
+        db.session.commit()
+
+        return redirect(url_for("index"))
+
+    return render_template("make-post.html", form=form)
 
 
 @app.route('/about')
