42aaec0b2688bf4b369f369a2325fc72c26d82ab
Alan Ramos López <118926925+ARandomGuyZero@users.noreply.github.com>
2024-11-01 13:36:14 -0700

    Add files via upload
    Added a Register Form

---
+++ forms.py
@@ -0,0 +1,20 @@
+from flask_ckeditor import CKEditorField
+from flask_wtf import FlaskForm
+from wtforms.fields.simple import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, URL, Email
+
+
+class PostForm(FlaskForm):
+    title = StringField("Blog Post Title:", validators=[DataRequired()])
+    subtitle = StringField("Blog Post Subtitle:", validators=[DataRequired()])
+    author = StringField("Your Name:", validators=[DataRequired()])
+    img_url = StringField("The Image URL for your Blog:", validators=[DataRequired(), URL()])
+    body = CKEditorField("Body:", validators=[DataRequired()])
+    submit = SubmitField()
+
+
+class RegisterForm(FlaskForm):
+    email = StringField("Email:", validators=[DataRequired(), Email()])
+    password = PasswordField("Password:", validators=[DataRequired()])
+    name = StringField("Name")
+    submit = SubmitField()
--- main.py
+++ main.py
@@ -13,14 +13,14 @@ from smtplib import SMTP
 
 from flask import Flask, render_template, request, url_for
 from flask_bootstrap import Bootstrap5
-from flask_ckeditor import CKEditor, CKEditorField
+from flask_ckeditor import CKEditor
 from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import FlaskForm
 from sqlalchemy import Integer, String, Text
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from werkzeug.security import generate_password_hash
 from werkzeug.utils import redirect
-from wtforms.fields.simple import StringField, SubmitField
-from wtforms.validators import DataRequired, URL
+
+from forms import PostForm, RegisterForm
 
 EMAIL = "email@gmail.com"  # Your email
 PASSWORD = "password"  # Your password
@@ -47,15 +47,6 @@ db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
 
-class PostForm(FlaskForm):
-    title = StringField("Blog Post Title:", validators=[DataRequired()])
-    subtitle = StringField("Blog Post Subtitle:", validators=[DataRequired()])
-    author = StringField("Your Name:", validators=[DataRequired()])
-    img_url = StringField("The Image URL for your Blog:", validators=[DataRequired(), URL()])
-    body = CKEditorField("Body:", validators=[DataRequired()])
-    submit = SubmitField()
-
-
 # CONFIGURE TABLE
 class BlogPost(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
@@ -65,6 +56,17 @@ class BlogPost(db.Model):
     body: Mapped[str] = mapped_column(Text, nullable=False)
     author: Mapped[str] = mapped_column(String(250), nullable=False)
     img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(250), nullable=False)
+    name: Mapped[str] = mapped_column(String(250), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
@@ -172,6 +174,31 @@ def delete_post(index_number):
     db.session.commit()
 
     return redirect(url_for("index"))
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    # Creates a new form using the UserForm class
+    form = RegisterForm()
+    if request.method == "POST":
+        # Generate a password using a hashed method
+        hashed_password = generate_password_hash(request.form.get("password"), method="pbkdf2:sha256", salt_length=8)
+
+        new_user = User(
+            email=request.form.get("email"),
+            password=hashed_password,
+            name=request.form.get("name")
+        )
+
+        # Add a new user to the database
+        db.session.add(new_user)
+
+        # Save the data to the database
+        db.session.commit()
+
+        return redirect(url_for("index"))
+
+    return render_template("register.html", form=form)
 
 
 @app.route('/about')
